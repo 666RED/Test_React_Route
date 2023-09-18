ab152956804e70d1ff4b5d65bb5586ab96604aa1
666RED <hongchensee8@gmail.com>
2023-09-18 22:47:27 +0800

    all changes done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Setup from "./project/react-router";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  );
 };
 
 export default App;
